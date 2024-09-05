7e77c25f55e4df5abc26678ff7c570287221036d
megait <megait@gmail.com>
2024-09-05 21:50:15 +0900

    [우재현]  - 기초 파일 생성 완료  - SNS 개인 프로젝트 회원가입 메인 화면 구현중

---
+++ src/main/java/com/jhsns/Sns/user/UserController.java
@@ -0,0 +1,15 @@
+package com.jhsns.Sns.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	@GetMapping("/join-view")
+	public String inputJoin()
+	{
+		return "user/join";
+	}
+}
